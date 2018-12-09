cf97a3992cff0da3e04cd0632dcde3b69ad36c36
sonmi451 <aishamclean451@gmail.com>
2018-12-09 23:39:12 +0000

    added section for xpath selectors, put startup of webdriver in function

--- src/test/e2e/arrays_challenge.py
+++ src/test/e2e/arrays_challenge.py
@@ -14,6 +14,10 @@ from selenium.webdriver.common.keys import Keys
 ################################################################################
 # VARIABLES ####################################################################
 
+# Test site
+webpage = "http://localhost:3000/"
+
+# name for submission
 name = "Aisha McLean"
 
 # set debug mode
@@ -25,11 +29,37 @@ if len(sys.argv) > 1 and sys.argv[1] == "debug":
 test_array = [10, 15, 5, 7, 1, 24, 36, 2] # answer = index 5, value 24
 
 ################################################################################
+# XPATH ########################################################################
+
+render_challenge_button = "//button[@data-test-id='render-challenge']"
+challenge_section = "//section[@id='challenge']/div/div/h1"
+array_table = "//*[@id='challenge']/div/div/div[1]/div/div[2]/table"
+tr_1 = "//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[1]"
+tr_2 = "//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[2]"
+tr_3 = "//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[3]"
+input_answer_1 = "//*[@data-test-id='submit-1']"
+input_answer_2 = "//*[@data-test-id='submit-2']"
+input_answer_3 = "//*[@data-test-id='submit-3']"
+input_answer_4 = "//*[@data-test-id='submit-4']"
+submit_button = "//*[@id='challenge']/div/div/div[2]/div/div[2]/button"
+
+################################################################################
 # FUNCTIONS ####################################################################
 
-# Once you have each array, write a function that is able to return the index of the array
-# where the sum of integers at the index on the left is equal to the sum of integers on the right.
-# If there is no index return null
+# function to startup webdriver
+def start_webdriver(env):
+    # start driver
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 100)
+    # go to webpage
+    driver.get(env)
+    print("On page: ",driver.title)
+    return driver
+
+# function to fulfil challenge requirements
+# > Once you have each array, write a function that is able to return the index of the array
+# > where the sum of integers at the index on the left is equal to the sum of integers on the right.
+# > If there is no index return null
 
 def array_center_finder(array):
     answer = None
@@ -55,11 +85,11 @@ def array_center_finder(array):
 # function to submit test results
 def submit_results(answer_1, answer_2, answer_3):
     # get elements
-    answer_space_1 = driver.find_element_by_xpath("//*[@data-test-id='submit-1']")
-    answer_space_2 = driver.find_element_by_xpath("//*[@data-test-id='submit-2']")
-    answer_space_3 = driver.find_element_by_xpath("//*[@data-test-id='submit-3']")
-    answer_space_4 = driver.find_element_by_xpath("//*[@data-test-id='submit-4']")
-    submit = driver.find_element_by_xpath("//*[@id='challenge']/div/div/div[2]/div/div[2]/button")
+    answer_space_1 = driver.find_element_by_xpath(input_answer_1)
+    answer_space_2 = driver.find_element_by_xpath(input_answer_2)
+    answer_space_3 = driver.find_element_by_xpath(input_answer_3)
+    answer_space_4 = driver.find_element_by_xpath(input_answer_4)
+    submit = driver.find_element_by_xpath(submit_button)
     # fill answers
     answer_space_1.send_keys(answer_1)
     answer_space_2.send_keys(answer_2)
@@ -70,42 +100,26 @@ def submit_results(answer_1, answer_2, answer_3):
     print("\nSubmitted answers and name: ",answer_1,answer_2,answer_3,name)
 
 ################################################################################
-# WEBDRIVER ####################################################################
-
-# Test site
-webpage = "http://localhost:3000/"
-
-# Driver
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 100)
-
-################################################################################
 # RUN SCRIPT ###################################################################
 
-driver.get(webpage)
-print("Opening page: ",driver.title)
+driver = start_webdriver(webpage)
 
 try:
-    render_challenge = driver.find_element_by_xpath("//button[@data-test-id='render-challenge']")
+    render_challenge = driver.find_element_by_xpath(render_challenge_button)
     render_challenge.send_keys(Keys.RETURN)
 
     # Challenge
-    challenge = driver.find_elements_by_xpath("//section[@id='challenge']/div/div/h1")
+    challenge = driver.find_elements_by_xpath(challenge_section)
     challenge_title = challenge[0].get_attribute("innerText")
     print("\nSection heading: ", challenge_title)
 
     # Read in whole data table
-    table = driver.find_element_by_xpath("//*[@id='challenge']/div/div/div[1]/div/div[2]/table")
+    table = driver.find_element_by_xpath(array_table)
 
-    # read in each row
-    array_1 = driver.find_element_by_xpath("//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[1]").get_attribute("innerText").split()
-    array_2 = driver.find_element_by_xpath("//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[2]").get_attribute("innerText").split()
-    array_3 = driver.find_element_by_xpath("//*[@id='challenge']/div/div/div[1]/div/div[2]/table/tbody/tr[3]").get_attribute("innerText").split()
-
-    # convert to ints
-    array_1 = [int(i) for i in array_1]
-    array_2 = [int(i) for i in array_2]
-    array_3 = [int(i) for i in array_3]
+    # read in each row as ints
+    array_1 = [int(i) for i in driver.find_element_by_xpath(tr_1).get_attribute("innerText").split()]
+    array_2 = [int(i) for i in driver.find_element_by_xpath(tr_2).get_attribute("innerText").split()]
+    array_3 = [int(i) for i in driver.find_element_by_xpath(tr_3).get_attribute("innerText").split()]
 
     # find center of array
     if debug:
